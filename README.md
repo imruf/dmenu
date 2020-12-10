c555c13454fbd370a1b429ba07b8a65e065cf508
imrufbd <imrufbd@gmail.com>
2020-12-10 22:00:07 +0600

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+dmenu - dynamic menu
+---
+dmenu is an efficient dynamic menu for X.
+
+
+In This Build Of dmenu
+---
+
+Use ctrl+vim keys to select suggestions.
+
--- config.def.h
+++ config.def.h
@@ -2,11 +2,12 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:bold:pixelsize=14:antialias=true:autohint=true"
+	"monospace:bold:pixelsize=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
