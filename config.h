dfe8fe9eb2190fbe131076d3f19c0ff348334ce0
imrufbd <imrufbd@gmail.com>
2021-07-06 00:17:03 +0600

    new config

--- config.h
+++ config.h
@@ -5,8 +5,9 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
+	/* "Comic Mono:bold:pixelsize=12:antialias=true:autohint=true" */
 static const char *fonts[] = {
-	"Comic Mono:bold:pixelsize=12:antialias=true:autohint=true"
+	"mono:pixelsize=14:antialias=true:autohint=true"
 };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
